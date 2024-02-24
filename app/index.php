02cd5638dc649b7f2d6239937d37f65e2378a41f
luke <ivan1.krasikov.2003@gmail.com>
2024-02-24 14:46:23 +0300

    Создание директории app и перенос в него файлов приложения

---
+++ app/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang=ru>
+<head>
+    <meta charset="UTF-8">
+    <title>Выбор ЛР</title>
+</head>
+<body>
+    <h>Выбор ЛР</h>
+    <a href="lab1/lr1.php">Лабораторная работа №1</a>
+</body>
+</html>
